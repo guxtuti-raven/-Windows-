bf18ff0f2de7d3fbe1bc0556a6524ce05d3af841
坂口 祐稀 <33012932+guxtuti-raven@users.noreply.github.com>
2019-10-11 02:31:44 +0900

    作業分のアップロード

---
+++ Split/Split1/Split1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Split1
+{
+    public partial class Form1 : Form
+    {
+        urlaccess access = new urlaccess();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String url = textBox1.Text;
+            if (url == "")
+                MessageBox.Show("テキストボックス空白です。\nURLを入力してください", "値が無効です", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                access.accessCheck(url);
+        }
+    }
+}
---
+++ Split/Split1/Split1/urlaccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Split1
+{
+    class urlaccess
+    {
+        public void accessCheck(String url)
+        {
+            String access_url = null;
+            String[] access_code = new String[2];
+
+            //URIの有効性の確認
+            Uri uriResult;
+            bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            if (result)
+            {
+                //WebRequestの作成
+                System.Net.HttpWebRequest webreq = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+
+                System.Net.HttpWebResponse webres = null;
+                try
+                {
+                    //サーバーからの応答を受信するためのWebResponseを取得
+                    webres = (System.Net.HttpWebResponse)webreq.GetResponse();
+
+                    //応答したURIを表示する
+                    access_url = webres.ResponseUri.ToString();
+                    //応答ステータスコードを表示する
+                    access_code[0] = webres.StatusCode.ToString();
+                    Console.WriteLine(webres.StatusCode + ", " + webres.StatusDescription);
+                    access_code[1] = webres.StatusDescription;
+                }
+                catch (System.Net.WebException ex)
+                {
+                    //HTTPプロトコルエラーかどうか調べる
+                    if (ex.Status == System.Net.WebExceptionStatus.ProtocolError)
+                    {
+                        //HttpWebResponseを取得
+                        System.Net.HttpWebResponse errres = (System.Net.HttpWebResponse)ex.Response;
+                        //応答したURIを表示する
+                        access_url = errres.ResponseUri.ToString();
+                        //応答ステータスコードを表示する
+                        access_code[0] = errres.StatusCode.ToString();
+                        access_code[1] = errres.StatusDescription;
+                    }
+                }
+                finally
+                {
+                    //閉じる
+                    if (webres != null)
+                        webres.Close();
+                }
+
+                MessageBox.Show("接続URL: " + access_url + "\n" + "HTTPステータス: " + access_code[0] + " " + access_code[1], "HTTP接続結果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("有効なURLではありません。", "確認してください", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
